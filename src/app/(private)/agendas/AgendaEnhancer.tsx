d78efdc7699e08a2ffcdd6b0d6fbed13cb6d072c
L34n7 <86571901+L34n7@users.noreply.github.com>
2026-07-27 03:08:15 -0300

    Alinhar ações de criação da agenda à direita

--- src/app/(private)/agendas/AgendaEnhancer.tsx
+++ src/app/(private)/agendas/AgendaEnhancer.tsx
@@ -5,9 +5,26 @@ import AgendaAutomationRuntimeStatus from "./AgendaAutomationRuntimeStatus";
 import AgendaEnhancerLegacy from "./AgendaEnhancerLegacy";
 import AgendaGoogleAgendaBindingFix from "./AgendaGoogleAgendaBindingFix";
 
+const HEADER_ACTION_ALIGNMENT_STYLES = `
+  .agendaTemplateShell .a2 .head.agendaHeadPremium .agendaRefreshBtn {
+    margin-left: 0 !important;
+  }
+
+  .agendaTemplateShell .a2 .head.agendaHeadPremium .agendaNewBtn {
+    margin-left: auto !important;
+  }
+
+  @media (max-width: 860px) {
+    .agendaTemplateShell .a2 .head.agendaHeadPremium .agendaNewBtn {
+      margin-left: 0 !important;
+    }
+  }
+`;
+
 export default function AgendaEnhancer() {
   return (
     <>
+      <style>{HEADER_ACTION_ALIGNMENT_STYLES}</style>
       <AgendaEnhancerLegacy />
       <AgendaAutomationEnhancer />
       <AgendaAutomationRuntimeStatus />
