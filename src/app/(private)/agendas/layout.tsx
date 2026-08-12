ac4d87a5660b4d55bca9de2943c58fc60fd728ac
L34n7 <86571901+L34n7@users.noreply.github.com>
2026-08-12 14:21:40 -0300

    Corrige layout e scroll do modal de configuração

--- src/app/(private)/agendas/layout.tsx
+++ src/app/(private)/agendas/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import { garantirPermissaoPagina } from "@/lib/permissoes/servidor";
 import "./agenda-layout.css";
+import "./agenda-config-modal.css";
 
 export default async function AgendasLayout({ children }: { children: ReactNode }) {
   await garantirPermissaoPagina("agendas.visualizar");
